94a53161ae84b90515fd8d0fd2749721acc13085
siikamiika <siikamiika@users.noreply.github.com>
2021-09-23 02:15:32 +0300

    add fcitx

--- linux/arch/custom/TODO_fcitx/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/.local/share/fcitx5/inputmethod
-cp keyboard-autism-common.conf ~/.local/share/fcitx5/inputmethod/
---
+++ linux/arch/custom/fcitx/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p ~/.config/fcitx5/conf
+cp profile ~/.config/fcitx5/profile
+cp config ~/.config/fcitx5/config
+find conf/ -maxdepth 1 -type f -exec cp "{}" ~/.config/fcitx5/conf/ \;
+
+mkdir -p ~/.local/share/fcitx5/inputmethod
+mkdir -p ~/.local/share/fcitx5/rime
+cp keyboard-autism-common.conf ~/.local/share/fcitx5/inputmethod/
+cp default.custom.yaml ~/.local/share/fcitx5/rime/
