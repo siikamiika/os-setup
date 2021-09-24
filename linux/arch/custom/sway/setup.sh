b95f0d4397e521d6a43692c7af8ee0c23d8e6d8d
siikamiika <siikamiika@users.noreply.github.com>
2021-09-24 18:16:03 +0300

    add sway gestures

--- linux/arch/custom/sway/setup.sh
+++ linux/arch/custom/sway/setup.sh
@@ -2,4 +2,5 @@
 
 echo "Configuring sway"
 ln -sf "$(pwd)/config" ~/.config/sway/config
-sudo cp get_focus_prompt /usr/local/bin/get_focus_prompt
+sudo cp get_focus_prompt /usr/local/bin/
+sudo cp sway_gesture_input /usr/local/bin/
