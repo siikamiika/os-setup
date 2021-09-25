80b5fd438b4eb8ad58a4098d7fdab4aaac924822
siikamiika <siikamiika@users.noreply.github.com>
2021-09-25 17:58:55 +0300

    more ime env variables

--- linux/arch/custom/env/setup.sh
+++ linux/arch/custom/env/setup.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 echo "Configuring environment"
+
+# default software
 grep -qF 'EDITOR="/usr/bin/nvim"' /etc/environment || echo 'EDITOR="/usr/bin/nvim"' | sudo tee -a /etc/environment > /dev/null
+
+# firefox
 grep -qF 'MOZ_ENABLE_WAYLAND=1' /etc/environment || echo 'MOZ_ENABLE_WAYLAND=1' | sudo tee -a /etc/environment > /dev/null
+
+# fcitx
 grep -qF 'GTK_IM_MODULE=fcitx' /etc/environment || echo 'GTK_IM_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
 grep -qF 'QT_IM_MODULE=fcitx' /etc/environment || echo 'QT_IM_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
+grep -qF 'GLFW_IM_MODULE=fcitx' /etc/environment || echo 'GLFW_IM_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
+grep -qF 'INPUT_METHOD=fcitx' /etc/environment || echo 'INPUT_METHOD=fcitx' | sudo tee -a /etc/environment > /dev/null
 grep -qF 'XMODIFIERS=@im=fcitx' /etc/environment || echo 'XMODIFIERS=@im=fcitx' | sudo tee -a /etc/environment > /dev/null
+grep -qF 'IMSETTINGS_MODULE=fcitx' /etc/environment || echo 'IMSETTINGS_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
+
+# ui toolkits
 grep -qF 'QT_QPA_PLATFORM=wayland' /etc/environment || echo 'QT_QPA_PLATFORM=wayland' | sudo tee -a /etc/environment > /dev/null
 grep -qF 'QT_WAYLAND_DISABLE_WINDOWDECORATION="1"' /etc/environment || echo 'QT_WAYLAND_DISABLE_WINDOWDECORATION="1"' | sudo tee -a /etc/environment > /dev/null
 grep -qF '_JAVA_AWT_WM_NONREPARENTING=1' /etc/environment || echo '_JAVA_AWT_WM_NONREPARENTING=1' | sudo tee -a /etc/environment > /dev/null
