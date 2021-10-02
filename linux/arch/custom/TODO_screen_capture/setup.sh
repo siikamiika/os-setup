f01158a4f66606d78b78e80bc3a0a4ca7cd0b9a1
siikamiika <siikamiika@users.noreply.github.com>
2021-10-02 19:51:52 +0300

    add screen recorder

--- linux/arch/custom/TODO_screen_capture/setup.sh
+++ linux/arch/custom/TODO_screen_capture/setup.sh
@@ -3,6 +3,8 @@
 echo "Configuring screen capture"
 
 mkdir -p ~/Pictures/screenshots
+mkdir -p ~/Videos/screenrecords
 
 sudo cp sway_select_area /usr/local/bin/
 sudo cp screenshot-area /usr/local/bin/
+sudo cp screenrecord-area /usr/local/bin/
