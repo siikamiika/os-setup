e75923cd26ae4dc5111df572c3964dcfc00a491e
siikamiika <siikamiika@users.noreply.github.com>
2021-10-05 03:41:18 +0300

    download more ram

--- linux/arch/custom/zram/setup.sh
+++ linux/arch/custom/zram/setup.sh
@@ -3,4 +3,5 @@
 echo "Configuring zram"
 sudo modprobe zram
 sudo cp zram-generator.conf /etc/systemd/
+sudo cp 99-swappiness.conf /etc/sysctl.d/
 sudo systemctl start systemd-zram-setup@zram0.service
