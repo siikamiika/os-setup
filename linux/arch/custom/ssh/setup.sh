31c92f9a5d5e0cf0308dcb16019126285f3d23e8
siikamiika <siikamiika@users.noreply.github.com>
2021-10-06 20:24:21 +0300

    add sshd

--- linux/arch/custom/ssh/setup.sh
+++ linux/arch/custom/ssh/setup.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
 echo "Configuring SSH"
-ssh-keygen -t rsa -b 4096 -a 100
+[ ! -e ~/.ssh/id_rsa ] && ssh-keygen -t rsa -b 4096 -a 100
+
+read -p "Enable sshd? [y/N]" -n 1 -r && echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    read -p "Disable SSH password login and root login? [y/N]" -n 1 -r && echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        sudo sed \
+            -e '/^#\?PermitRootLogin prohibit-password$/c PermitRootLogin no' \
+            -e '/^#\?PasswordAuthentication yes$/c PasswordAuthentication no' \
+            -e '/^#\?KbdInteractiveAuthentication yes$/c KbdInteractiveAuthentication no' \
+            -e '/^#\?UsePAM yes$/c UsePAM no' \
+            -i /etc/ssh/sshd_config
+        systemctl is-active --quiet sshd.service && sudo systemctl reload sshd.service
+    fi
+
+    sudo systemctl enable sshd.service
+    sudo systemctl start sshd.service
+fi
