e880f2d40ae5c6e87af7c3b586b0d4126f940cbe
siikamiika <siikamiika@users.noreply.github.com>
2021-10-16 19:01:52 +0300

    add fzf-launcher

--- linux/arch/custom/sway/setup.sh
+++ linux/arch/custom/sway/setup.sh
@@ -4,3 +4,4 @@ echo "Configuring sway"
 ln -sf "$(pwd)/config" ~/.config/sway/config
 sudo cp get_focus_prompt /usr/local/bin/
 sudo cp sway_gesture_input /usr/local/bin/
+sudo cp fzf-launcher /usr/local/bin/
