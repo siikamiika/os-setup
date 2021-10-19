e202b13e66deaca8a3fc9202ccda18337ed1da0a
siikamiika <siikamiika@users.noreply.github.com>
2021-10-19 06:23:03 +0300

    add sway_focus_pid

--- linux/arch/custom/sway/setup.sh
+++ linux/arch/custom/sway/setup.sh
@@ -2,6 +2,7 @@
 
 echo "Configuring sway"
 ln -sf "$(pwd)/config" ~/.config/sway/config
-sudo cp get_focus_prompt /usr/local/bin/
-sudo cp sway_gesture_input /usr/local/bin/
-sudo cp fzf-launcher /usr/local/bin/
+sudo cp ./get_focus_prompt /usr/local/bin/
+sudo cp ./sway_gesture_input /usr/local/bin/
+sudo cp ./fzf-launcher /usr/local/bin/
+sudo cp ./sway_focus_pid /usr/local/bin/
