c5ddbd0b51e28766f6c5fc2b6945e8925b9d0200
siikamiika <siikamiika@users.noreply.github.com>
2021-10-20 23:26:05 +0300

    add backup cron

--- linux/arch/custom/backup/setup.sh
+++ linux/arch/custom/backup/setup.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
+echo "Configuring backup"
 sudo cp ./rsync-inc-backup.sh /usr/local/bin/
+
+CRONTAB="/var/spool/cron/$USER"
+sudo cp ./crontab "$CRONTAB"
+sudo chown "$USER:$USER" "$CRONTAB"
+sudo chmod 600 "$CRONTAB"
+
+echo "Remember to set the BACKUP_HOST and BACKUP_HOST_DIR variables"
+
+sudo systemctl enable cronie.service
+sudo systemctl restart cronie.service
