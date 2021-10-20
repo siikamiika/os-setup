a116bbd941a90a8fda2aa58a2487ffe82ed32ca2
siikamiika <siikamiika@users.noreply.github.com>
2021-10-20 22:56:08 +0300

    simplify env

--- linux/arch/custom/env/setup.sh
+++ linux/arch/custom/env/setup.sh
@@ -2,27 +2,29 @@
 
 echo "Configuring environment"
 
-# default software
-grep -qF 'EDITOR="/usr/bin/nvim"' /etc/environment || echo 'EDITOR="/usr/bin/nvim"' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'DMENU="/usr/local/bin/fzf-launcher"' /etc/environment || echo 'DMENU="/usr/local/bin/fzf-launcher"' | sudo tee -a /etc/environment > /dev/null
+sudo cp ./upsert_env /usr/local/bin/
 
-# firefox
-grep -qF 'MOZ_ENABLE_WAYLAND=1' /etc/environment || echo 'MOZ_ENABLE_WAYLAND=1' | sudo tee -a /etc/environment > /dev/null
+# # default software
+upsert_env EDITOR /usr/bin/nvim
+upsert_env DMENU /usr/local/bin/fzf-launcher
 
-# fcitx
-grep -qF 'XIM=fcitx' /etc/environment || echo 'XIM=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'XIM_PROGRAM=fcitx' /etc/environment || echo 'XIM_PROGRAM=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'GTK_IM_MODULE=fcitx' /etc/environment || echo 'GTK_IM_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'QT_IM_MODULE=fcitx' /etc/environment || echo 'QT_IM_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'GLFW_IM_MODULE=fcitx' /etc/environment || echo 'GLFW_IM_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'INPUT_METHOD=fcitx' /etc/environment || echo 'INPUT_METHOD=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'XMODIFIERS=@im=fcitx' /etc/environment || echo 'XMODIFIERS=@im=fcitx' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'IMSETTINGS_MODULE=fcitx' /etc/environment || echo 'IMSETTINGS_MODULE=fcitx' | sudo tee -a /etc/environment > /dev/null
+# # firefox
+upsert_env MOZ_ENABLE_WAYLAND 1
 
-# ui toolkits
-grep -qF 'QT_QPA_PLATFORM=wayland' /etc/environment || echo 'QT_QPA_PLATFORM=wayland' | sudo tee -a /etc/environment > /dev/null
-grep -qF 'QT_WAYLAND_DISABLE_WINDOWDECORATION="1"' /etc/environment || echo 'QT_WAYLAND_DISABLE_WINDOWDECORATION="1"' | sudo tee -a /etc/environment > /dev/null
-grep -qF '_JAVA_AWT_WM_NONREPARENTING=1' /etc/environment || echo '_JAVA_AWT_WM_NONREPARENTING=1' | sudo tee -a /etc/environment > /dev/null
+# # fcitx
+upsert_env XIM fcitx
+upsert_env XIM_PROGRAM fcitx
+upsert_env GTK_IM_MODULE fcitx
+upsert_env QT_IM_MODULE fcitx
+upsert_env GLFW_IM_MODULE fcitx
+upsert_env INPUT_METHOD fcitx
+upsert_env XMODIFIERS @im=fcitx
+upsert_env IMSETTINGS_MODULE fcitx
 
-# screen sharing
-grep -qF 'XDG_CURRENT_DESKTOP=sway' /etc/environment || echo 'XDG_CURRENT_DESKTOP=sway' | sudo tee -a /etc/environment > /dev/null
+# # ui toolkits
+upsert_env QT_QPA_PLATFORM wayland
+upsert_env QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+upsert_env _JAVA_AWT_WM_NONREPARENTING 1
+
+# # screen sharing
+upsert_env XDG_CURRENT_DESKTOP sway
