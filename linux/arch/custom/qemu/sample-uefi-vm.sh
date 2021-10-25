fcef83332befc93071776d6c2080a91eac32cc93
siikamiika <siikamiika@users.noreply.github.com>
2021-10-25 19:11:38 +0300

    update qemu uefi vm sample

--- linux/arch/custom/qemu/sample-uefi-vm.sh
+++ linux/arch/custom/qemu/sample-uefi-vm.sh
@@ -6,6 +6,8 @@
 qemu-system-x86_64 \
     -enable-kvm \
     -m 1G \
+    -net user,hostfwd=tcp::2222-:22 \
+    -net nic \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2-ovmf/x64/OVMF.fd \
     -drive format=raw,file=./drive.img \
     -cdrom ./os.iso \
