b3ff42b53aaa316d7f71a04f01d74b6a067364a7
siikamiika <siikamiika@users.noreply.github.com>
2021-11-07 05:02:52 +0200

    wireguard cleanup

--- linux/arch/custom/TODO_vpn/setup.sh
+++ linux/arch/custom/TODO_vpn/setup.sh
@@ -2,36 +2,6 @@
 
 # TODO ipv6
 
-interface_tmpl()
-{
-    # MTU = 1420
-    echo "\
-[Interface]
-Address = $1
-ListenPort = $2
-PrivateKey = $3"
-}
-
-CONF_FILE=/etc/wireguard/wg0.conf
-
-[ ! "$(id -u)" -eq 0 ] && echo "must run as root" && exit 1
-
 echo "Configuring wireguard"
 
-if [ -e "$CONF_FILE" ]; then
-    exit
-fi
-
-ADDRESS=$(while true; do
-    read -p "Address (CIDR): "
-    [ ! -z "$REPLY" ] && echo $REPLY && break
-done)
-LISTEN_PORT=$(while true; do
-    read -p "ListenPort: "
-    [ ! -z "$REPLY" ] && echo $REPLY && break
-done)
-PRIVKEY="$(wg genkey)"
-(umask 0077 && interface_tmpl "$ADDRESS" "$LISTEN_PORT" "$PRIVKEY" > "$CONF_FILE")
-
-groupadd -f wireguard_users
-sudo systemctl enable --now wg-quick@wg0
+sudo groupadd -f wireguard_users
