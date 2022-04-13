0cc9ccad73b598981adf9726dfe8cbe26b9c4013
siikamiika <siikamiika@users.noreply.github.com>
2022-04-13 20:47:02 +0300

    change dragon keybind

---
+++ linux/arch/custom/ranger/update_patch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+update_or_create_patch.sh /usr/share/doc/ranger/config/rc.conf ~/.config/ranger/rc.conf .
+update_or_create_patch.sh /usr/share/doc/ranger/config/rifle.conf ~/.config/ranger/rifle.conf .
