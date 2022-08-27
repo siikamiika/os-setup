c8602150f3f8cb9772b307df4bce22b7bd10c7cf
siikamiika <siikamiika@users.noreply.github.com>
2022-08-28 02:18:22 +0300

    add defaults

---
+++ macos/defaults/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Configuring macOS defaults"
+
+# set key repeat rate and delay
+defaults write -g InitialKeyRepeat -int 20 # 300 ms
+defaults write -g KeyRepeat -int 1 # 15 ms (2 would be 30 ms)
+
+# enable key repeat on letter keys
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# disable autocorrect
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
