b253d2d8975bd328d43d79ded176aefa99910052
siikamiika <siikamiika@users.noreply.github.com>
2023-01-06 17:57:46 +0200

    fix ranger patch updater for Darwin

--- linux/arch/custom/ranger/update_patch.sh
+++ linux/arch/custom/ranger/update_patch.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
-update_or_create_patch.sh /usr/share/doc/ranger/config/rc.conf ~/.config/ranger/rc.conf .
-update_or_create_patch.sh /usr/share/doc/ranger/config/rifle.conf ~/.config/ranger/rifle.conf .
+#!/usr/bin/env bash
+
+os="$(uname -s)"
+case "$os" in
+    Linux)
+        ranger_config="/usr/share/doc/ranger/config"
+        ;;
+    Darwin)
+        ranger_config="/opt/homebrew/opt/ranger/libexec/ranger/config"
+        ;;
+    *)
+        echo "Unknown os: $os"
+        exit 1
+        ;;
+esac
+
+update_or_create_patch.sh "$ranger_config/rc.conf" ~/.config/ranger/rc.conf .
+update_or_create_patch.sh "$ranger_config/rifle.conf" ~/.config/ranger/rifle.conf .
